b102cead3785557d9faa5d0566cebd22dafa9a1c
Geer68 <germhidalgo@gmail.com>
2025-01-19 17:52:02 -0300

    modificaciones (?

--- interfaces/clientes.ts
+++ interfaces/clientes.ts
@@ -6,3 +6,18 @@ export interface ClienteCardProps {
   category: string;
   year: string;
 }
+
+export interface Cliente {
+  name: string;
+  image: string;
+  logo: string;
+  href: string;
+  category: string;
+  year: string;
+  description: React.ReactNode;
+  startingPoint: React.ReactNode;
+  stats: {
+    title: string;
+    description: string;
+  }[];
+}
