23bbf2c1751863c7a9f7d526f98db9a2348bb607
Geer68 <germhidalgo@gmail.com>
2025-01-19 17:01:50 -0300

    ga4

--- components/transformacion/TrasnformationComplete.tsx
+++ components/transformacion/TrasnformationComplete.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import { Timeline } from "@/components/ui/timeline";
-import { Sparkle } from "lucide-react";
 import { pasos } from "@/constants/transformation/complete";
 
 export default function TransformationComplete() {
